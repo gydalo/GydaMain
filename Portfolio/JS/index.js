d6fd9b60cc956db1f48825e1a324d47763fc45ea
gydalo <gylo_l@hotmail.com>
2024-06-07 12:02:13 +0200

    gallery

--- Portfolio/JS/index.js
+++ Portfolio/JS/index.js
@@ -1,28 +1,3 @@
-const gameHub = document.querySelector("#gameHub");
-
-gameHub.addEventListener("click", function() {
-    window.location.href = "https://gydalo.github.io/GydaMain/CA/";
-})
-
-
-const csm = document.querySelector("#csm");
-
-csm.addEventListener("click", function() {
-    window.location.href = "https://gydalo.github.io/GydaMain/SP1/index.html";
-})
-
-const rainyDays = document.querySelector("#rainyDays");
-
-rainyDays.addEventListener("click", function() {
-    window.location.href = "https://gydalo.github.io/GydaMain/CA-JS1/";
-})
-
-const hotView = document.querySelector("#hotView");
-
-hotView.addEventListener("click", function() {
-    window.location.href = "https://norofffeu.github.io/FED1-PE1-gydalo/index.html";
-})
-
 
 const aboutMe = document.querySelector("#aboutMe");
 
