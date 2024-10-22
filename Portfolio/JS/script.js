018474a55ceb738fd747a66c3f409269c60a6c11
gydalo <gylo_l@hotmail.com>
2024-10-22 12:36:34 +0200

    Banner

--- Portfolio/JS/script.js
+++ Portfolio/JS/script.js
@@ -8,36 +8,3 @@ function toggleMenu() {
 
 
 
-
-
-
-const gameHub = document.querySelector("#gameHub");
-
-gameHub.addEventListener("click", function() {
-    window.open("https://gydalo.github.io/GydaMain/CA/");
-})
-
-const csm = document.querySelector("#csm");
-
-csm.addEventListener("click", function() {
-    window.open("https://gydalo.github.io/GydaMain/SP1/index.html");
-})
-
-const rainyDays = document.querySelector("#rainyDays");
-
-rainyDays.addEventListener("click", function() {
-    window.open("https://gydalo.github.io/GydaMain/CA-JS1/");
-})
-
-const hotView = document.querySelector("#hotView");
-
-hotView.addEventListener("click", function() {
-    window.open("https://norofffeu.github.io/FED1-PE1-gydalo/index.html");
-})
-
-const github = document.querySelector("#github");
-
-github.addEventListener("click", function() {
-    window.open("https://github.com/gydalo");
-})
-
