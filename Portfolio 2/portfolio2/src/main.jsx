fcdb09dc220488a915f2fe8978cd86648f1d8d34
gydalo <gylo_l@hotmail.com>
2025-06-01 18:03:52 +0200

    Links to projectpages made

--- Portfolio 2/portfolio2/src/main.jsx
+++ Portfolio 2/portfolio2/src/main.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProjectAaryn from "./pages/projects/Aaryn";
+import ProjectBiddaroo from "./pages/projects/Biddaroo";
+import ProjectHolidaze from "./pages/projects/Holidaze";
 import "./css/style.css";
 import "./css/mediaqueries.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects/Aaryn" element={<ProjectAaryn />} />
+        <Route path="/projects/Biddaroo" element={<ProjectBiddaroo />} />
+        <Route path="/projects/Holidaze" element={<ProjectHolidaze />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
+
 );
---
+++ Portfolio 2/portfolio2/src/pages/projects/Biddaroo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../../components";
+
+function ArticlePage() {
+    return (
+      <Layout>
+
+      </Layout>
+    );
+  }
+
+  export default ArticlePage;
