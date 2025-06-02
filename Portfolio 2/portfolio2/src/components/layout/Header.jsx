130b8e4ca6de37755eb02d639e6804203e43fa36
gydalo <gylo_l@hotmail.com>
2025-06-02 12:38:58 +0200

    Header changes when on projectpages, scrolls to top when click into a new page

--- Portfolio 2/portfolio2/src/components/layout/Header.jsx
+++ Portfolio 2/portfolio2/src/components/layout/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => setMenuOpen((prev) => !prev);
   const closeMenu = () => setMenuOpen(false);
@@ -17,6 +18,11 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const isProjectPage =
+    location.pathname.includes("/projects/Aaryn") ||
+    location.pathname.includes("/projects/Biddaroo") ||
+    location.pathname.includes("/projects/Holidaze");
+
   return (
     <header className={`header ${scrolled ? "scrolled" : ""}`} id="header">
       {/* Desktop Nav */}
@@ -26,18 +32,34 @@ const Header = () => {
           <span className="logo-frontend">Frontend Developer</span>
         </Link>
         <ul className="nav-links">
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+          {isProjectPage ? (
+            <>
+              <li>
+                <Link to="/projects/Aaryn">Aaryn</Link>
+              </li>
+              <li>
+                <Link to="/projects/Biddaroo">Biddaroo</Link>
+              </li>
+              <li>
+                <Link to="/projects/Holidaze">Holidaze</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
 
@@ -57,26 +79,48 @@ const Header = () => {
             <span></span>
           </div>
           <ul className={`menu-links ${menuOpen ? "open" : ""}`}>
-            <li className="menu-link">
-              <a href="#about" onClick={closeMenu}>
-                About
-              </a>
-            </li>
-            <li className="menu-link">
-              <a href="#experience" onClick={closeMenu}>
-                Experience
-              </a>
-            </li>
-            <li className="menu-link">
-              <a href="#projects" onClick={closeMenu}>
-                Projects
-              </a>
-            </li>
-            <li className="menu-link">
-              <a href="#contact" onClick={closeMenu}>
-                Contact
-              </a>
-            </li>
+            {isProjectPage ? (
+              <>
+                <li className="menu-link">
+                  <Link to="/projects/Aaryn" onClick={closeMenu}>
+                    Aaryn
+                  </Link>
+                </li>
+                <li className="menu-link">
+                  <Link to="/projects/Biddaroo" onClick={closeMenu}>
+                    Biddaroo
+                  </Link>
+                </li>
+                <li className="menu-link">
+                  <Link to="/projects/Holidaze" onClick={closeMenu}>
+                    Holidaze
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="menu-link">
+                  <a href="#about" onClick={closeMenu}>
+                    About
+                  </a>
+                </li>
+                <li className="menu-link">
+                  <a href="#experience" onClick={closeMenu}>
+                    Experience
+                  </a>
+                </li>
+                <li className="menu-link">
+                  <a href="#projects" onClick={closeMenu}>
+                    Projects
+                  </a>
+                </li>
+                <li className="menu-link">
+                  <a href="#contact" onClick={closeMenu}>
+                    Contact
+                  </a>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </nav>
--- Portfolio 2/portfolio2/src/main.jsx
+++ Portfolio 2/portfolio2/src/main.jsx
@@ -5,12 +5,14 @@ import HomePage from "./pages/HomePage";
 import ProjectAaryn from "./pages/projects/Aaryn";
 import ProjectBiddaroo from "./pages/projects/Biddaroo";
 import ProjectHolidaze from "./pages/projects/Holidaze";
+import ScrollToTop from "./components/ScrollToTop";
 import "./css/style.css";
 import "./css/mediaqueries.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects/Aaryn" element={<ProjectAaryn />} />
